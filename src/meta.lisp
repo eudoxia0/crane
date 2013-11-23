c5a1143338dad64331ca1a97626de9ca63b9e2fd
Fernando Borretti <eudoxiahp@gmail.com>
2013-11-23 21:48:44 -0200

    Add primary key slot option

--- src/meta.lisp
+++ src/meta.lisp
@@ -29,28 +29,33 @@
   ((col-type :initarg :col-type
              :reader  col-type)
    (col-null-p :initarg :col-null-p
-             :reader  col-null-p
-             :initform t)
+               :reader  col-null-p
+               :initform t)
    (col-unique-p :initarg :col-unique-p
-             :reader  col-unique-p
-             :initform nil)
+                 :reader  col-unique-p
+                 :initform nil)
+   (col-primary-p :initarg :col-primary-p
+                  :reader col-primary-p
+                  :initform nil)
    (col-index-p :initarg :col-index-p
-             :reader  col-index-p
-             :initform nil)
+                :reader  col-index-p
+                :initform nil)
    (col-default :initarg :col-default
-             :reader  col-default)))
+                :reader  col-default)))
 
 (defclass table-class-effective-slot-definition (closer-mop:standard-effective-slot-definition)
   ((col-type :initarg :col-type
-         :accessor col-type)
+             :accessor col-type)
    (col-null-p :initarg :col-null-p
-             :reader  col-null-p)
+               :reader  col-null-p)
    (col-unique-p :initarg :col-unique-p
-             :reader  col-unique-p)
+                 :reader  col-unique-p)
+   (col-primary-p :initarg :col-primary-p
+                  :reader col-primary-p)
    (col-index-p :initarg :col-index-p
-             :reader  col-index-p)
+                :reader  col-index-p)
    (col-default :initarg :col-default
-             :reader  col-default)))
+                :reader  col-default)))
 
 ;;; Common Lisp is a vast ocean of possibilities, stretching infinitely
 ;;; and with no horizon... And here I am pretending to understand
@@ -76,7 +81,10 @@
           
           (slot-value effective-slot-definition 'col-unique-p)
           (col-unique-p (first direct-slot-definitions))
-          
+
+          (slot-value effective-slot-definition 'col-primary-p)
+          (col-primary-p (first direct-slot-definitions))
+
           (slot-value effective-slot-definition 'col-index-p)
           (col-index-p (first direct-slot-definitions))
           
@@ -94,6 +102,7 @@
                   :type (col-type slot)
                   :null-p (col-null-p slot)
                   :unique-p (col-null-p slot)
+                  :primar-p (col-primar-p slot)
                   :index-p (col-index-p slot)
                   :default (col-default slot)))))
 
@@ -113,3 +122,8 @@ See DIGEST."
   (mapcar #'diff-slot
           (sort (second digest-a) #'string<=)
           (sort (second digest-b) #'string<=)))
+
+(defmethod initialize-instance :after ((class table-class) &key)
+  (if (migration-history-p (table-name class))
+      (format t "Class already defined~&")
+      (format t "Class defined for the first time~&")))
