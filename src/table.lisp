ebbfd7552fc911737649e36dc824dad5f74db268
Fernando Borretti <eudoxiahp@gmail.com>
2013-11-23 10:32:56 -0200

    Update table.lisp

--- src/table.lisp
+++ src/table.lisp
@@ -1,17 +1,22 @@
 (in-package :crane)
 
+(defun process-slot (slot)
+  slot)
+
 (defun separate-slots-and-options (slots-and-options)
   (let ((slots (list))
         (options (list)))
     (loop for item in slots-and-options do
       (if (eq (symbol-package (car item)) (find-package :keyword))
           (push item options)
-          (push item slots)))
+          (push (process-slot item) slots)))
     (list slots options)))
 
 (defmacro deftable (name (&rest superclasses) &rest slots-and-options)
   (destructuring-bind (slots options) (separate-slots-and-options slots-and-options)
-    `(defclass ,name ,superclasses
-       ,slots
-       ,@options
-       (:metaclass crane:table-class))))
+    `(progn
+       (defclass ,name ,superclasses
+         ,slots
+         ,@options
+         (:metaclass crane:table-class))
+       (closer-mop:ensure-class (find-class (quote ,name))))))
