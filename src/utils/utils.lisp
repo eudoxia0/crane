81827092edbc90eee8bddb60ddadce4e6f240707
Fernando Borretti <eudoxiahp@gmail.com>
2013-11-23 11:01:16 -0200

    Add slurp-file util

--- src/utils/utils.lisp
+++ src/utils/utils.lisp
@@ -16,3 +16,11 @@ the result as a list of ([property] [old value] [new value])"
             (if (funcall test val-a val-b)
                 t
                 (list slot val-a val-b)))))))
+
+@export
+(defun slurp-file (path)
+  ;; Credit: http://www.ymeme.com/slurping-a-file-common-lisp-83.html
+  (with-open-file (stream path)
+    (let ((seq (make-array (file-length stream) :element-type 'character :fill-pointer t)))
+      (setf (fill-pointer seq) (read-sequence seq stream))
+      seq)))
