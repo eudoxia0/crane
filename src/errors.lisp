a4dd1fbe9d47b41c38433d533c3b95613db3d31d
Fernando Borretti <eudoxiahp@gmail.com>
2013-12-07 17:14:48 -0200

    Add empty-table errors, rearrange others

--- src/errors.lisp
+++ src/errors.lisp
@@ -3,9 +3,14 @@
 (in-package :crane.errors)
 (annot:enable-annot-syntax)
 
-@export
-(define-condition configuration-error (error)
+(define-condition crane-error ()
   ((text :initarg :text :reader text)))
 
 @export
+(define-condition configuration-error (crane-error) ())
+  
+@export
 (define-condition no-configuration-error (configuration-error) ())
+
+@export
+(define-condition empty-table (crane-error) ())
