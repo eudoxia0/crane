80eed762a44396cb60cb7f155885a830e0b028bc
Fernando Borretti <eudoxiahp@gmail.com>
2013-12-11 18:24:00 -0200

    Add docs

--- src/table.lisp
+++ src/table.lisp
@@ -1,16 +1,21 @@
 (in-package :crane)
 (annot:enable-annot-syntax)
 
+@doc "Take a plist like (:col-type 'string :col-null-p t) and remove the
+prefixes on the keys."
 (defun process-slot (slot)
   slot)
 
+@doc "To minimize the number of parentheses, both slots and table options come
+in the same list. This function separates them: Normal slot names are plain old
+symbols, table options are keywords."
 (defun separate-slots-and-options (slots-and-options)
   (let ((slots (list))
         (options (list)))
     (iter (for item in slots-and-options)
       (if (eq (symbol-package (car item)) (find-package :keyword))
           (push item options)
-          (push (process-slot item) slots)))
+          (push (process-slot (cdr item)) slots)))
     (list slots options)))
 
 @export
