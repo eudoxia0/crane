aca295e9d8920dbf5edb2d7cb52e96f6cf7f6877
Fernando Borretti <eudoxiahp@gmail.com>
2013-12-14 11:57:19 -0200

    Add connection tests

--- t/connect.lisp
+++ t/connect.lisp
@@ -4,28 +4,67 @@
   `(crane::validate-connection-spec
     :test-db ,db-type ,spec))
 
-(defsuite postgres
+(defun process-specs (db-type test-cases)
+  (cons 'progn
+        (loop for case in test-cases collecting
+              `(validate ,db-type (quote ,case)))))
+
+(defmacro shared-bad-specs (db-type)
+  "Bad connection specs shared by both PostgreSQL and MySQL."
+  (let ((test-cases
+          '(()
+            (:name "foo")
+            (:user "john" :pass "1234")
+            (:name "foo" :user "john")
+            (:name "foo" :some-other-key t)
+            (:some-other-key t))))
+    (process-specs db-type test-cases)))
+
+(defmacro shared-good-specs (db-type)
+  "Valid connection specs for PostgreSQL and MySQL."
+  (let ((test-cases
+          '((:name "foo" :user "john" :pass "1234"))))
+    (process-specs db-type test-cases)))
+
+(def-suite postgres
   :description "SQLite3 connection spec tests.")
 (in-suite postgres)
 
 (test bad-specs
-  (signals 'crane.errors:configuration-error
-    (validate :postgres '())))
+  (signals crane.errors:configuration-error
+    (shared-bad-specs :postgres)))
 
-(defsuite sqlite3
+(test good-specs
+  (finishes
+    (shared-good-specs :postgres)))
+
+(def-suite sqlite3
   :description "SQLite3 connection spec tests.")
 (in-suite sqlite3)
 
 (test bad-specs
   (signals 'crane.errors:configuration-error
-    (validate :sqlite3 '())))
+    (validate :sqlite3 '())
+    (validate :sqlite3 '(:some-other-key t))))
 
-(defsuite mysql
+(test bad-specs
+  (finishes
+    (validate :sqlite3 '(:name "my-db"))))
+
+
+(def-suite mysql
   :description "SQLite3 connection spec tests.")
 (in-suite mysql)
 
 (test bad-specs
-  (signals 'crane.errors:configuration-error
-    (validate :mysql '())))
+  (signals crane.errors:configuration-error
+    (shared-bad-specs :mysql)))
+
+(test good-specs
+  (finishes
+    (shared-good-specs :mysql)))
 
 
+(run! 'postgres)
+(run! 'sqlite3)
+(run! 'mysql)
