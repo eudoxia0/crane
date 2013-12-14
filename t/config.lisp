81ece5b51993b58f2d7d21bf7d4947848307aa30
Fernando Borretti <eudoxiahp@gmail.com>
2013-12-14 11:05:51 -0200

    Fix configuration error

--- t/config.lisp
+++ t/config.lisp
@@ -6,17 +6,17 @@
   `(:application-root
     (merge-pathnames #p"t/"
                      ,(asdf:component-pathname (asdf:find-system :crane-test)))
-    :crane `(:migrations-directory
-             ,(merge-pathnames
-               #p"t/migrations/"
-               (asdf:component-pathname (asdf:find-system :crane-test)))
-             :databases
-             (:main
-              (:type :sqlite3
-               :name ":memory:")))))
+    :crane (:migrations-directory
+              ,(merge-pathnames
+                #p"t/migrations/"
+                (asdf:component-pathname (asdf:find-system :crane-test)))
+            :databases
+              (:main
+               (:type :sqlite3
+                :name ":memory:")))))
 
 (defconfig |dev|
-  (list
-   :debug t))
+  `(:debug t
+    ,@|common|))
 
 (setf (env-var "CRANE_TEST_ENV") "dev")
