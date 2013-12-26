fdfaa496fcd9e39cc3d7f94f37083c564a258ce9
Fernando Borretti <eudoxiahp@gmail.com>
2013-12-26 20:38:40 -0200

    Delete some code, have to rethink this

--- src/sql.lisp
+++ src/sql.lisp
@@ -9,18 +9,6 @@
 (in-package :crane.sql)
 (annot:enable-annot-syntax)
 
-(defun make-constraint (column-name cons-type value)
-  (format nil "CONSTRAINT ~A_~A ~A ~A"
-          column-name cons-type cons-type value))
-
-
-@export
-(defun process-column-constraints (column)
-  (iter (for key in '(:nullp :uniquep :primaryp :indexp))
-    (collecting (make-constraint (getf column :name)
-                                 key
-                                 (getf column key)))))
-
 @export
 (defun sqlize (obj)
   (typecase obj
