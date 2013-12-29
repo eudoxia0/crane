784cc56489ae7126ab55f5fb4fb67e5f873ae03d
Fernando Borretti <eudoxiahp@gmail.com>
2013-12-29 12:15:02 -0200

    Indentation and other changes

--- t/meta.lisp
+++ t/meta.lisp
@@ -6,19 +6,19 @@
 
 
 (def-suite table-slots
-    :description "Test that table metaclass slots work.")
+  :description "Test that table metaclass slots work.")
 (in-suite table-slots)
 
 (test create-tables
   (finishes
     (deftable table-a ()
-      (field-a :type 'string :nullp t))
+      (field-a :type string :nullp t))
     
     (deftable table-b (table-a)
       (:abstractp t))
     
     (deftable table-c (table-a)
-      (field-b :type 'integer :nullp nil :indexp t)
+      (field-b :type integer :nullp nil :indexp t)
       (:abstractp t)
       (:table-name table--c))))
     
@@ -35,13 +35,13 @@
 
 
 (def-suite column-slots
-    :description "Test that table column options work.")
+  :description "Test that table column options work.")
 (in-suite column-slots)
 
 (test column-options
   (finishes
     (deftable table-d (table-a)
-      (field-b :type 'integer :default 1
+      (field-b :type integer :default 1
                :unique-p t)))
   (finishes
     (closer-mop:class-slots (find-class 'table-d)))
