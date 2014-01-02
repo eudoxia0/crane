b8d8f0970a113b56948d674e962de67db344e452
Fernando Borretti <eudoxiahp@gmail.com>
2014-01-02 09:51:52 -0200

    Add some utility functions

--- src/interface.lisp
+++ src/interface.lisp
@@ -4,6 +4,27 @@
 (in-package :crane)
 (annot:enable-annot-syntax)
 
+(defmethod slot-tuple (object)
+  (mapcar #'symbol-name
+          (remove-if-not #'(lambda (slot)
+                             (slot-boundp object slot))
+                         (mapcar #'closer-mop:slot-definition-name
+                                 (closer-mop:class-slots (class-of object))))))
+
+@doc "Transform an object into a tuple of its values. Useful for INSERT
+statements."
+(defmethod tuple (object)
+  (mapcar #'(lambda (slot)
+              (slot-value object slot))
+          (remove-if-not #'(lambda (slot)
+                             (slot-boundp object slot))
+                         (mapcar #'closer-mop:slot-definition-name
+                                 (closer-mop:class-slots (class-of object))))))
+
+@export
+(defmethod save (object)
+  )
+
 @export
 (defmethod filter ((class table-class) &rest params)
   
