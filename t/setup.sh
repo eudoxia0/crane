5bd91913ba0fd04d3de2b1a490759e26ddcb7d28
Fernando Borretti <eudoxiahp@gmail.com>
2014-01-18 15:39:12 -0200

    Improve setup.sh

--- t/setup.sh
+++ t/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo -u postgres dropdb crane_test_db
 sudo -u postgres createdb crane_test_db
-sudo -u postgres psql -c "CREATE USER crane_test_user WITH PASSWORD 'crane_test_user';
-GRANT ALL PRIVILEGES ON DATABASE crane_test_db TO crane_test_user"
+sudo -u postgres psql -c "CREATE USER crane_test_user WITH PASSWORD 'crane_test_user'"
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE crane_test_db TO crane_test_user"
