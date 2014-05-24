ce163f4b195001e5f9a6fa8277553be2b9d710d8
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-24 18:25:47 -0300

    Better connection tests

--- t/connect.lisp
+++ t/connect.lisp
@@ -4,40 +4,54 @@
   `(crane.connect::validate-connection-spec
     :test-db ,db-type ,spec))
 
-(defun process-specs (db-type test-cases)
-  (cons 'progn
-        (loop for case in test-cases collecting
-              `(validate ,db-type (quote ,case)))))
-
-(defmacro shared-bad-specs (db-type)
-  "Bad connection specs shared by both PostgreSQL and MySQL."
-  (let ((test-cases
+(defmacro generate-bad-tests (db-type)
+  (let ((specs
           '(()
             (:name "foo")
             (:user "john" :pass "1234")
             (:name "foo" :user "john")
             (:name "foo" :some-other-key t)
             (:some-other-key t))))
-    (process-specs db-type test-cases)))
+    `(progn
+       ,@(loop for spec in specs
+               for i from 0 to (1- (length specs))
+               collecting
+               `(test ,(intern (concatenate 'string
+                                            "bad-spec-"
+                                            (write-to-string i))
+                               *package*)
+                  (signals crane.errors:configuration-error
+                    (validate ,db-type ',spec)))))))
 
-(defmacro shared-good-specs (db-type)
-  "Valid connection specs for PostgreSQL and MySQL."
-  (let ((test-cases
+(defmacro generate-good-tests (db-type)
+  (let ((specs
           '((:name "foo" :user "john" :pass "1234"))))
-    (process-specs db-type test-cases)))
-
+    `(progn
+       ,@(loop for spec in specs
+               for i from 0 to (1- (length specs))
+               collecting
+               `(test ,(intern (concatenate 'string
+                                            "good-spec-"
+                                            (write-to-string i))
+                               *package*)
+                  (finishes
+                    (validate ,db-type ',spec)))))))
 
 (def-suite postgres
-  :description "SQLite3 connection spec tests.")
+  :description "Postgres connection spec tests.")
 (in-suite postgres)
 
-(test bad-specs
-  (signals crane.errors:configuration-error
-    (shared-bad-specs :postgres)))
+(generate-bad-tests :postgres)
+(generate-good-tests :postgres)
 
-(test good-specs
-  (finishes
-    (shared-good-specs :postgres)))
+
+(def-suite mysql
+  :description "MySQL connection spec tests.")
+(in-suite mysql)
+
+
+(generate-bad-tests :mysql)
+(generate-good-tests :mysql)
 
 
 (def-suite sqlite3
@@ -54,18 +68,6 @@
     (validate :sqlite3 '(:name "my-db"))))
 
 
-(def-suite mysql
-  :description "SQLite3 connection spec tests.")
-(in-suite mysql)
-
-(test bad-specs
-  (signals crane.errors:configuration-error
-    (shared-bad-specs :mysql)))
-
-(test good-specs
-  (finishes
-    (shared-good-specs :mysql)))
-
 (def-suite connecting
     :description "Test that we can actually connect.")
 (in-suite connecting)
@@ -77,7 +79,8 @@
 (test main-db
   (is (equal :main crane.connect:*default-db*)))
 
+
 (run! 'postgres)
-(run! 'sqlite3)
 (run! 'mysql)
+(run! 'sqlite3)
 (run! 'connecting)
