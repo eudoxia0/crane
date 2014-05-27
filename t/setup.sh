6f57111212456498bede64394f90518a51defcc9
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-27 19:45:27 -0300

    Remove setup script

--- t/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo -u postgres dropdb crane_test_db
-sudo -u postgres createdb crane_test_db
-sudo -u postgres psql -c "CREATE USER crane_test_user WITH PASSWORD 'crane_test_user'"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE crane_test_db TO crane_test_user"
