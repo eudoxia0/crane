d753cc49554e0a86cfc4c9cf0c23642e10707d00
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-27 19:45:15 -0300

    Set up Postgres database

--- t/provision/common.sh
+++ t/provision/common.sh
@@ -36,3 +36,22 @@ EOF
 
 echo $LISP_INIT > ~/.sbclrc
 echo $SOURCE_REGISTRY > ~/.config/common-lisp/source-registry.conf
+
+### Set up databases
+
+## Postgres
+
+sudo -u postgres createdb crane_test_db
+sudo -u postgres psql -c \
+    "CREATE USER crane_test_user WITH PASSWORD 'crane_test_user'"
+sudo -u postgres psql -c \
+    "GRANT ALL PRIVILEGES ON DATABASE crane_test_db TO crane_test_user"
+
+## MySQL
+
+### Run the tests
+
+###...
+
+### Tear down
+sudo -u postgres dropdb crane_test_db
