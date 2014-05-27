2e9276d225f62cee4da723a15bee82ee0777fbe0
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-27 19:31:50 -0300

    Move files.sh and quicklisp.sh to common.sh

---
+++ t/provision/common.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+### Install Quicklisp
+
+QLFILE=/tmp/ql.lisp
+QLURL=http://beta.quicklisp.org/quicklisp.lisp
+
+if [[ ! -f ~/quicklisp/setup.lisp ]]; then
+    echo "Installing Quicklisp"
+    curl -o $QLFILE $QLURL
+    sbcl --no-sysinit --no-userinit --noprint --noinform \
+         --load $QLFILE \
+         --eval '(quicklisp-quickstart:install)' \
+         --quit
+fi
+
+### Add configuration files
+
+echo "Copying configuration files"
+
+read -d '' LISP_INIT <<"EOF"
+(setf *print-case* :downcase)
+
+#-quicklisp
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
+                                       (user-homedir-pathname))))
+  (when (probe-file quicklisp-init)
+    (load quicklisp-init)))
+EOF
+
+echo $LISP_INIT > ~/.sbclrc
