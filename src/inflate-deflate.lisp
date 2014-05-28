e388c5b814d9ee98afa4c527a31f22105c37a339
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-28 19:34:11 -0300

    Terminology confusion 3:

--- src/inflate-deflate.lisp
+++ src/inflate-deflate.lisp
@@ -1,8 +1,7 @@
-;;;; Inflation/deflation map CLOS objects to SQL strings and vice-versa. This is
-;;;; unrelated to the ORM, and meant to allow complex column datatypes to be
-;;;; mapped to CLOS objects. For example, mapping SQL timestamps to structures
-;;;; that represent time, or mapping other more complex SQL types to CLOS
-;;;; objects.
+;;;; Inflation/deflation map SQL string to CLOS objects. This is unrelated to
+;;;; the ORM, and meant to allow complex column datatypes to be mapped to CLOS
+;;;; objects. For example, mapping SQL timestamps to structures that represent
+;;;; time, or mapping other more complex SQL types to CLOS objects.
 (in-package :cl-user)
 (defpackage :crane.inflate-deflate
   (:use :cl :anaphora)
@@ -12,20 +11,20 @@
            :defdeflate))
 (in-package :crane.inflate-deflate)
 
-(defgeneric inflate (obj)
+(defgeneric deflate (obj)
   (:documentation "Turn a CLOS object into a string for insertion in the database."))
 
-(defmacro definflate ((obj-name obj-type) &rest body)
+(defmacro defdeflate ((obj-name obj-type) &rest body)
   `(defmethod crane.inflate-deflate:inflate ((,obj-name ,obj-type))
      ,@body))
 
-(definflate (str string) str)
-(definflate (num number) num)
+(defdeflate (str string) str)
+(defdeflate (num number) num)
 
-(defgeneric deflate (obj type-name)
+(defgeneric inflate (obj type-name)
   (:documentation "Turn a string into a CLOS object."))
 
-(defmacro defdeflate ((obj-name obj-type-name) &rest body)
-  `(defmethod crane.inflate-deflate:deflate (,obj-name
+(defmacro definflate ((obj-name obj-type-name) &rest body)
+  `(defmethod crane.inflate-deflate:inflate (,obj-name
                                              (type (eql ,obj-type-name)))
      ,@body))
