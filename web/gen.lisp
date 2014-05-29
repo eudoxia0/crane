de37bb2eca003ca3a7ce4952340d3b01829d07b3
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-29 20:29:54 -0300

    Add configuration example

--- web/gen.lisp
+++ web/gen.lisp
@@ -40,6 +40,9 @@
 (register-emb "snippet" (merge-pathnames
                        #p"web/templates/snippet.lisp"
                        +crane-path+))
+(register-emb "config" (merge-pathnames
+                       #p"web/templates/config.lisp"
+                       +crane-path+))
 (register-emb "features" (merge-pathnames
                           #p"web/templates/features.tmpl"
                           +crane-path+))
---
+++ web/templates/config.lisp
@@ -0,0 +1,13 @@
+(setup
+ `(:migrations-directory
+   ,(merge-pathnames
+     #p"migrations/"
+     (asdf:system-source-directory :myapp)
+   :databases
+   (:main
+    (:type :postgres
+     :name "myapp_db"
+     :user "user"
+     :pass "user"))))
+
+(connect)
