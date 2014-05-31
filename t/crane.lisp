152dff3d3f820ff727ff8ff347ac87e3d5b42e3b
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-31 12:03:49 -0300

    Remove dead code

--- t/crane.lisp
+++ t/crane.lisp
@@ -2,10 +2,3 @@
 (defpackage crane-test
   (:use :cl :crane :fiveam))
 (in-package :crane-test)
-
-;;;; This has to be done before connecting
-(let ((command
-        (namestring (merge-pathnames
-                     #p"t/setup.sh"
-                     (asdf:component-pathname (asdf:find-system :crane))))))
-  (asdf:run-shell-command command))
