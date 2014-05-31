872ae70d50a30269160534ededa7e76ef1c18396
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-31 12:02:02 -0300

    Separate provisioning from test-running

--- t/provision/common.sh
+++ t/provision/common.sh
@@ -34,22 +34,3 @@ EOF
 echo $LISP_INIT > ~/.sbclrc
 mkdir -p ~/.config/common-lisp
 echo $SOURCE_REGISTRY > ~/.config/common-lisp/source-registry.conf
-
-### Set up databases
-
-## Postgres
-
-sudo -u postgres dropdb crane_test_db
-sudo -u postgres createdb crane_test_db
-sudo -u postgres psql -c \
-    "CREATE USER crane_test_user WITH PASSWORD 'crane_test_user'"
-sudo -u postgres psql -c \
-    "GRANT ALL PRIVILEGES ON DATABASE crane_test_db TO crane_test_user"
-
-## MySQL
-
-### Run the tests
-sbcl --eval '(ql:quickload :crane-test)' --quit > /home/vagrant/crane/report.txt
-
-### Tear down
-sudo -u postgres dropdb crane_test_db
