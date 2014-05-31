4acf00b27a330aa0840306629682895fac0e8716
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-30 22:35:23 -0300

    Update config example

--- web/examples/config.lisp
+++ web/examples/config.lisp
@@ -1,13 +1,13 @@
 (setup
- `(:migrations-directory
-   ,(merge-pathnames
-     #p"migrations/"
-     (asdf:system-source-directory :myapp)
-   :databases
-   (:main
-    (:type :postgres
-     :name "myapp_db"
-     :user "user"
-     :pass "user"))))
+ :migrations-directory
+ (merge-pathnames
+  #p"migrations/"
+  (asdf:system-source-directory :myapp))
+ :databases
+ '(:main
+   (:type :postgres
+    :name "myapp_db"
+    :user "user"
+    :pass "user")))
 
 (connect)
