c8ed101d0cbda38b79a0f03abc229fdfa059e554
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-31 12:07:29 -0300

    Merge relations tests into table tests
    Rename meta.lisp to table.lisp
    
    Move test.sh to a better location

--- run_tests.sh
+++ run_tests.sh
@@ -2,7 +2,7 @@
 
 function test() {
     vagrant up $1
-    vagrant ssh $1 -c "sudo su -c 'bash crane/t/provision/test.sh'"
+    vagrant ssh $1 -c "sudo su -c 'bash crane/t/test.sh'"
     #vagrant halt $1
 }
 
--- t/table.lisp
+++ t/table.lisp
@@ -49,6 +49,27 @@
     (closer-mop:class-slots (find-class 'table-d)))
   (finishes (crane.meta:digest (find-class 'table-d))))
 
+(def-suite foreign
+  :description "Testing the 'R' in ORM.")
+(in-suite foreign)
+
+(test creating-related-tables
+  (finishes
+    (deftable parent-table ()
+      (something :type integer))
+   
+    (deftable child-table ()
+      (something-else :type text :initform "Foo")
+      (ref :type integer :foreign (parent-table :on-delete :cascade)))))
+
+(test (dereferencing :depends-on creating-related-tables)
+  (is (equal
+       (let* ((parent-instance (create 'parent-table :something 99))
+              (child-instance  (create 'child-table :ref (id parent-instance))))
+         (something (deref child-instance 'ref)))
+       99)))
+
 (run! 'preliminary)
 (run! 'table-slots)
 (run! 'column-slots)
+(run! 'foreign)
