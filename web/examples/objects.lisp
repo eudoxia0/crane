f78cc21d2f02fcfec180c75f5a0450a325169f24
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-30 21:17:36 -0300

    Fix objects example

--- web/examples/objects.lisp
+++ web/examples/objects.lisp
@@ -1,4 +1,10 @@
-(let ((ins (create 'user :name "Eudoxia")))
-  ;; CREATE implicity saves the object
-  (save ins)
-  (del ins))
+(let ((instance (create 'ship :name "River of Stars"
+                              :tonnage "77")))
+  ;; FIXME: It's back luck to rename a ship
+  (setf (name instance) "Serenity")
+  ;; Expand the cargo hold
+  (incf (tonnage instance) 25)
+  ;; Save these changes!
+  (save instance)
+  ;; Time to retire
+  (del instance))
