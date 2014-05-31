9ba74c4094e807b89084c6e979b3f6e5810c2341
Fernando Borretti <eudoxiahp@gmail.com>
2014-05-31 12:02:07 -0300

    Improve run_tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-vagrant up ubuntu
-vagrant provision ubuntu
-vagrant halt ubuntu
+function test() {
+    vagrant up $1
+    vagrant ssh $1 -c "sudo su -c 'bash crane/t/provision/test.sh'"
+    #vagrant halt $1
+}
+
+test ubuntu
