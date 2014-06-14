032945d9e7f43590aba41d4e0c1b8c3a84bd8621
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-13 22:23:13 -0300

    Test on Postgres and SQLite3

--- t/test.sh
+++ t/test.sh
@@ -14,7 +14,13 @@ sudo -u postgres psql -c \
 ## MySQL
 
 ### Run the tests
-sudo sbcl --eval '(ql:quickload :crane-test)' --quit
+
+## Postgres
+sudo sbcl --eval '(push :crane-use-postgres *features*)' \
+          --eval '(ql:quickload :crane-test)' --quit
+## SQLite3
+sudo sbcl --eval '(push :crane-use-sqlite3 *features*)' \
+          --eval '(ql:quickload :crane-test)' --quit
 
 ### Tear down
 sudo -u postgres dropdb crane_test_db
