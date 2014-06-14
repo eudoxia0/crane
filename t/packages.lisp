5ff3e8863281145d5a177c575d855877f73b2905
Fernando Borretti <eudoxiahp@gmail.com>
2014-06-14 11:24:14 -0300

    Add packages for other databases

--- t/packages.lisp
+++ t/packages.lisp
@@ -5,3 +5,11 @@
 (in-package :cl-user)
 (defpackage crane-test.postgres
   (:use :cl :crane :fiveam))
+
+(in-package :cl-user)
+(defpackage crane-test.mysql
+  (:use :cl :crane :fiveam))
+
+(in-package :cl-user)
+(defpackage crane-test.sqlite3
+  (:use :cl :crane :fiveam))
