fedc052f01c50c0008dca3a7f57502058d436135
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-12 16:17:43 -0200

    Fix a bug

--- t/final.lisp
+++ t/final.lisp
@@ -1,11 +1,18 @@
 (in-package :crane-test.final)
 
 (defmacro run-tests-in-context (major-suite)
-  `(progn
-     (run! ',major-suite)
-     ,@(loop for suite being the external-symbols of
-             (find-package :crane-test.generic) collecting
-             `(run! ',suite))))
+  (let ((test-suites (list :table-slots
+                           :column-slots
+                           :foreign
+                           :dereferencing
+                           :migrations
+                           :queries)))
+    `(progn
+       (run! ',major-suite)
+       ,@(loop for suite in test-suites
+               collecting
+               `(run! ',(intern (symbol-name suite)
+                                (find-package :crane-test.generic)))))))
 
 (run! 'crane-test.spec:postgres)
 (run! 'crane-test.spec:mysql)
