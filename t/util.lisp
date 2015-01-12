e1bc3def0d21b04833f9f60adb2d9ddb809b3e20
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-12 16:11:13 -0200

    Move t/util.lisp to t/utils.lisp, fix tests

---
+++ t/util.lisp
@@ -0,0 +1,14 @@
+(in-package :crane-test.util)
+
+(def-suite tests)
+(in-suite tests)
+
+(test diff-plist
+  (is
+   (equal
+    (crane.utils:diff-plist '(:a 1 :b 2 :c 3) '(:a 1 :b 2 :c 3))
+    nil))
+  (is
+   (equal
+    (crane.utils:diff-plist '(:a 1 :b 2 :c 3) '(:a 4 :b 5 :c 6))
+    '(:a (1 4) :b (2 5) :c (3 6)))))
