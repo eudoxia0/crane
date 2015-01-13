1ea0be67813c05386205f2cc97263b4aaf0c60c6
Fernando Borretti <eudoxiahp@gmail.com>
2015-01-13 08:35:50 -0200

    Run crane:setup within a test

--- t/postgres/config.lisp
+++ t/postgres/config.lisp
@@ -4,20 +4,22 @@
   :description "Test that we can configure Crane and connect to a DB.")
 (in-suite config)
 
-(crane:setup
- :debug t
- :migrations-directory
- (asdf:system-relative-pathname :crane-test #p"t/migrations/")
- :databases
- '(:postgres-db
-   (:type :postgres
-    :name "crane_test_db"
-    :user "crane_test_user"
-    :pass "crane_test_user")))
+(test setup
+  (finishes
+   (crane:setup
+    :debug t
+    :migrations-directory
+    (asdf:system-relative-pathname :crane-test #p"t/migrations/")
+    :databases
+    '(:postgres-db
+      (:type :postgres
+       :name "crane_test_db"
+       :user "crane_test_user"
+       :pass "crane_test_user")))))
 
-(test connect
+(test (connect :depends-on setup)
   (finishes
     (crane.connect:connect)))
 
-(test main-db
+(test (main-db :depends-on setup)
   (is (equal :postgres-db crane.connect:*default-db*)))
--- t/sqlite3/config.lisp
+++ t/sqlite3/config.lisp
@@ -4,20 +4,22 @@
   :description "Test that we can configure Crane and connect to a DB.")
 (in-suite config)
 
-(crane:setup
- :debug t
- :migrations-directory
- (merge-pathnames
-  #p"t/migrations/"
-  (asdf:system-source-directory :crane-test))
- :databases
- '(:sqlite-db
-   (:type :sqlite3
-    :name ":memory:")))
+(test setup
+  (finishes
+   (crane:setup
+    :debug t
+    :migrations-directory
+    (merge-pathnames
+     #p"t/migrations/"
+     (asdf:system-source-directory :crane-test))
+    :databases
+    '(:sqlite-db
+      (:type :sqlite3
+       :name ":memory:"))))
 
-(test main-db
+(test (main-db :depends-on setup)
   (is (equal :sqlite-db crane.connect:*default-db*)))
 
-(test connect
+(test (connect :depends-on main-db)
   (finishes
     (crane.connect:connect)))
