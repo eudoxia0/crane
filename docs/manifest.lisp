a7c38494f10e57b4ea5c2a9848a5afc78f19df89
Fernando Borretti <eudoxiahp@gmail.com>
2015-02-19 00:51:37 -0200

    Add the Codex manifest

---
+++ docs/manifest.lisp
@@ -0,0 +1,15 @@
+(:markup-format :vertex
+ :systems (:crane)
+ :documents ((:title "Crane Manual"
+              :authors ("Fernando Borretti")
+              :sources ("overview.tex"
+                        "connecting.tex"
+                        "tables.tex"
+                        "objects.tex"
+                        "queries.tex"
+                        "migrations.tex"
+                        "transactions.tex"
+                        "fixtures.tex"
+                        "inflate-deflate.tex"
+                        "appendix-a.tex"
+                        "appendix-b.tex"))))
