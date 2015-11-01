4cc83ba0caa3a9f763f9125b3b106b4135f3d6c3
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-01 19:59:00 -0300

    Add some tests

--- t/serialize.lisp
+++ t/serialize.lisp
@@ -6,3 +6,18 @@
 (def-suite serialization-tests
   :description "Serialization tests.")
 (in-suite serialization-tests)
+
+(test column
+  (is
+   (stringp
+    (yason:with-output-to-string* (:indent t)
+      (yason:encode-object
+       (first
+        (crane.table:table-columns (find-class 'crane-test.table::person))))))))
+
+(test table
+  (is
+   (stringp
+    (yason:with-output-to-string* (:indent t)
+      (yason:encode-object
+       (find-class 'crane-test.table::person))))))
