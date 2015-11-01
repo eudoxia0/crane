5145ab94419cab825323784d3623849e72667261
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-01 18:33:01 -0300

    Use classes to represent types

--- src/types.lisp
+++ src/types.lisp
@@ -1,8 +1,9 @@
 (in-package :cl-user)
 (defpackage crane.types
-  (:import-from :cl
-                :deftype)
-  (:export :int
+  (:use :cl)
+  ;; Classes
+  (:export :sql-type
+           :int
            :bigint
            :smallint
            :numeric
@@ -11,23 +12,86 @@
            :varchar
            :timestamp
            :datetime)
+  ;; Accessors
+  (:export :varchar-length)
+  ;; Interface
+  (:export :type-sql)
   (:documentation "Implements the database types."))
 (in-package :crane.types)
 
+;;; Interface
+
+(defclass sql-type ()
+  ()
+  (:documentation "The base class of all SQL types."))
+
+(defgeneric type-sql (type)
+  (:documentation "Return the SQL string that denotes a type.."))
+
+;;; Types
+
 ;; Numeric types
 
-(deftype int () `cl:integer)
-(deftype bigint () `cl:integer)
-(deftype smallint () `cl:integer)
-(deftype numeric () `cl:ratio)
-(deftype double () `cl:double-float)
+(defclass int (sql-type)
+  ()
+  (:documentation "An integer"))
+
+(defclass bigint (sql-type)
+  ()
+  (:documentation "A big integer."))
+
+(defclass smallint (sql-type)
+  ()
+  (:documentation "A small integer."))
+
+(defclass numeric (sql-type)
+  ()
+  (:documentation "A number."))
+
+(defclass double (sql-type)
+  ()
+  (:documentation "A double-precision floating-point number."))
 
 ;; Text
 
-(deftype text () `cl:string)
-(deftype varchar () `cl:string)
+(defclass text (sql-type)
+  ()
+  (:documentation "A piece of text."))
 
-;; Extra
+(defclass varchar (sql-type)
+  ((length :reader varchar-length
+           :initarg :length
+           :type integer
+           :documentation "The length of the string."))
+  (:documentation "A variable-length string."))
 
-(deftype timestamp () `cl:string)
-(deftype datetime () `cl:string)
+;; Time
+
+(defclass timestamp (sql-type)
+  ()
+  (:documentation "A timestamp."))
+
+(defclass datetime (sql-type)
+  ()
+  (:documentation "A date/time value."))
+
+;;; SQL
+
+(macrolet ((type-name (class name)
+             `(defmethod type-sql ((type ,class))
+                ,(format nil "Type string of the ~A type." (string-downcase class))
+                (declare (ignore type))
+                ,name)))
+  (type-name int "INTEGER")
+  (type-name bigint "BIGINT")
+  (type-name smallint "SMALLINT")
+  (type-name numeric "NUMERIC")
+  (type-name double "DOUBLE")
+  (type-name text "TEXT")
+  (type-name varchar "VARCHAR")
+  (type-name timestamp "TIMESTAMP")
+  (type-name datetime "DATETIME"))
+
+(defmethod type-sql ((type varchar))
+  "Type string of the varchar type."
+  (format nil "VARCHAR(~D)" (varchar-length type)))
