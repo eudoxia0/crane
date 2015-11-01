e4459137f7cb4f965539deb71b27fca4232e82af
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-01 18:34:34 -0300

    Delete some more files

--- src/fixture.lisp
+++
@@ -1,7 +0,0 @@
-(defpackage crane.fixture
-  (:use :cl)
-  (:documentation "Customizes clos-fixtures for use in Crane."))
-(in-package :crane.fixture)
-
-(defmethod clos-fixtures:register-fixture ((instance crane.table:<table>))
-  (crane.interface:create% instance))
