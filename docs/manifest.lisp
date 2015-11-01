54def0f54ad35a1f1622694250726a5e4cd35dc5
Fernando Borretti <eudoxiahp@gmail.com>
2015-11-01 19:03:44 -0300

    Add Codex documentation

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:crane)
+ :documents ((:title "Crane"
+              :authors ("Fernando Borretti")
+              :output-format (:type :multi-html
+                              :template :minima)
+              :sources ("manual.scr"))))
