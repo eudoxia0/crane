94c63f7682138595af87f0f233126ed0f932177d
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-07 20:58:28 -0300

    Some stub code for computing diffs between tables

---
+++ src/table/diff.lisp
@@ -0,0 +1,22 @@
+(in-package :cl-user)
+(defpackage crane.table.diff
+  (:use :cl)
+  (:import-from :crane.table
+                :table-class)
+  (:documentation "Tools for computing the difference between two table definitions."))
+(in-package :crane.table.diff)
+
+(defclass difference ()
+  ((new-columns :reader new-columns
+                :initarg :new-columns
+                :type list
+                :documentation "A list of column objects that are to be added the table.")
+   (old-columns :reader old-columns
+                :initarg :old-columns
+                :type list
+                :documentation "A list of column objects that are to be dropped."))
+  (:documentation "Represents the difference between two tables."))
+
+(defun differences (past present)
+  "Compute the differences between two table definitions."
+  (declare (type table-class past present)))
