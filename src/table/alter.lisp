627a7bbf08c7bb4ef865c8d4d85e363d82036f09
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-09 17:27:53 -0300

    Delete a couple of unused files

--- src/table/alter.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :cl-user)
-(defpackage crane.table.alter
-  (:use :cl)
-  (:documentation "Code to modify an existing a table."))
-(in-package :crane.table.alter)
--- src/table/create.lisp
+++
@@ -1,5 +0,0 @@
-(in-package :cl-user)
-(defpackage crane.table.create
-  (:use :cl)
-  (:documentation "Code to instantiate a table."))
-(in-package :crane.table.create)
