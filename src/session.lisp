1b03b0d8efa48a24a46119695f407694cf78e2ba
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-08 22:12:26 -0300

    Implement a bunch of session stuff, including table creation

--- src/session.lisp
+++ src/session.lisp
@@ -1,6 +1,20 @@
 (in-package :cl-user)
 (defpackage crane.session
   (:use :cl)
+  (:import-from :crane.config
+                :get-database)
+  (:import-from :crane.database
+                :database
+                :connect
+                :disconnect
+                :table-exists-p
+                :sql-query)
+  (:import-from :crane.table
+                :table-class
+                :table-name)
+  (:import-from :crane.table.sql
+                :make-table-definition
+                :table-definition-sql)
   (:export :session
            :make-session
            :session-databases
@@ -23,6 +37,7 @@
               databases this session connects to.")
    (tables :accessor session-tables
            :initarg :tables
+           :initform (make-hash-table :test #'eq)
            :type hash-table
            :documentation "A hash table from table names (symbols) to database
            tags (symbol). This associates the tables this sessin will manage
@@ -44,12 +59,16 @@
   (:documentation "A session ties table definitions, which are not tied to any
   particular database, to the actual databases where they will be created. It also manages the storage of migrations."))
 
-(defun make-session (&key databases migratep migrations-directory)
+(defun make-session (&key databases migratep (migrations-directory nil dirp))
   "Create a session object."
-  (make-instance 'session
-                 :databases databases
-                 :migratep migratep
-                 :migrations-directory migrations-directory))
+  (if dirp
+      (make-instance 'session
+                     :databases databases
+                     :migratep migratep
+                     :migrations-directory migrations-directory)
+      (make-instance 'session
+                     :migratep migratep
+                     :databases databases)))
 
 (defun register-database (session database-tag)
   "Add a database tag to a session, so a connection will be created when the
@@ -62,7 +81,7 @@ Returns the database tag."
   (push database-tag (session-databases session))
   database-tag)
 
-(defun register-table (session table-name database)
+(defun register-table (session table-name database-tag)
   "Register a table in the session, to be created in the given database.
 
 Returns the table name."
@@ -71,29 +90,46 @@ Returns the table name."
     effect until the session is restarted."))
   (when (gethash table-name (session-tables session))
     (warn "This table has already been mapped to a database in the session."))
-  (setf (gethash table-name (session-tables session)) database)
+  (setf (gethash table-name (session-tables session)) database-tag)
   table-name)
 
 (defmethod start ((session session))
   "Start the session, connecting to all the databases."
-  ;; Iterate over the databases, connecting them
-  (loop for tag in (session-databases session) do
-    (let ((db (crane.config:get-database tag)))
-      (when db
-        (crane.database:connect db))))
-  ;; Iterate over the tables, ensuring they exist
-  (loop for table-name being the hash-keys of (session-tables session) do
-    (let ((class (find-class table-name)))
-      (when class
-        ;; So we have a table-class object. If migrations are enabled, [LOL
-        ;; TODO]. If migrations are disabled, then we just have to check whether
-        ;; it exists in the database. If it exists, nothing has to be done. If
-        ;; it doesn't, we create it.
-        t))))
+  (with-slots (databases tables migratep) session
+    ;; Iterate over the databases, connecting them
+    (loop for tag in databases do
+      (let ((db (get-database tag)))
+        (when db
+          (crane.database:connect db))))
+    ;; Iterate over the tables, ensuring they exist
+    (loop for table-name being the hash-keys of tables
+          for database-tag being the hash-values of tables
+          do
+      (let ((table (find-class table-name))
+            (database (get-database database-tag)))
+        (when table
+          (if migratep
+              (error "Migrations not implemented yet :^)")
+              (progn
+                ;; Check if the table exists
+                (unless (table-exists-p database (table-name table))
+                  (format t "Creating table ~A" table-name)
+                  (create-table table database))))))))
+  (setf (session-started-p session) t)
+  nil)
 
 (defmethod stop ((session session))
   "Stop the session, closing all database connections."
   (loop for tag in (session-databases session) do
-    (let ((db (crane.config:get-database tag)))
+    (let ((db (get-database tag)))
       (when db
-        (crane.database:disconnect db)))))
+        (crane.database:disconnect db))))
+  (setf (session-started-p session) nil)
+  nil)
+
+(defmethod create-table ((table table-class) (database database))
+  "Create a table in a particular database."
+  (mapcar #'(lambda (sql)
+              (sql-query database sql nil))
+          (table-definition-sql
+           (make-table-definition table database))))
