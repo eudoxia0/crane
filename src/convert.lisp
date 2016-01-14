d7eb9fb6000bb1258762994f74dc6c14e3559045
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-13 21:02:54 -0300

    Fix conversion again

--- src/convert.lisp
+++ src/convert.lisp
@@ -31,10 +31,16 @@
 (defmethod lisp-to-database ((database crane.database:database)
                              (value local-time:timestamp))
   (declare (ignore database))
-  (local-time:timestamp-to-universal value))
+  (local-time:format-timestring nil value :timezone local-time:+utc-zone+))
 
 (defmethod database-to-lisp ((database crane.database:database)
                              (value integer)
                              (type crane.types:timestamp))
   (declare (ignore database type))
   (local-time:universal-to-timestamp value))
+
+(defmethod database-to-lisp ((database crane.database:database)
+                             (value string)
+                             (type crane.types:timestamp))
+  (declare (ignore database type))
+  (local-time:parse-timestring value))
