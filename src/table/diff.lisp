d56348df0a7d6c35794f2467f81034e36196d8fa
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-15 22:22:16 -0300

    Add some table diff code

--- src/table/diff.lisp
+++ src/table/diff.lisp
@@ -1,8 +1,16 @@
 (in-package :cl-user)
 (defpackage crane.table.diff
   (:use :cl)
+  (:import-from :crane.table
+                :column
+                :column-name)
   (:import-from :crane.table.serialize
-                :storable-table)
+                :storable-table
+                :table-columns)
+  (:export :difference
+           :new-columns
+           :old-columns
+           :differences)
   (:documentation "Tools for computing the difference between two table definitions."))
 (in-package :crane.table.diff)
 
@@ -17,8 +25,20 @@
                 :documentation "A list of column objects that are to be dropped."))
   (:documentation "Represents the difference between two tables."))
 
+(defun column= (a b)
+  (eq (column-name a) (column-name b)))
+
+(defun has-column-p (column table)
+  (not (null (position column table :test #'column=))))
+
 (defun differences (past present)
   "Compute the differences between two table definitions."
-  (declare (type storable-table past present)
-           (ignore past present))
-  t)
+  (declare (type storable-table past present))
+  (make-instance 'difference
+                 :new-columns (remove-if #'(lambda (column)
+                                             (has-column-p column (table-columns past)))
+                                         (table-columns present))
+                 :old-columns (remove-if #'(lambda (column)
+                                             (has-column-p column
+                                                           (table-columns present)))
+                                         (table-columns past))))
