90108d05936f82be389ec8d2656d863cd6bf6bc0
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-15 22:54:06 -0300

    Define some test tables

--- t/table/diff.lisp
+++ t/table/diff.lisp
@@ -6,3 +6,18 @@
 (def-suite diff-tests
   :description "Table diff tests.")
 (in-suite diff-tests)
+
+(crane.table:deftable alpha ()
+  ((a :type crane.types:int)
+   (b :type crane.types:int)
+   (c :type crane.types:int)))
+
+(crane.table:deftable beta ()
+  ((a :type crane.types:int)
+   (b :type crane.types:int)
+   (c :type crane.types:int)
+   (d :type crane.types:int)))
+
+(crane.table:deftable gamma ()
+  ((a :type crane.types:int)
+   (b :type crane.types:int)))
