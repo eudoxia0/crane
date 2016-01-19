28d17922a1e65757eac35b7c3d7c86c83ab6a070
Fernando Borretti <eudoxiahp@gmail.com>
2016-01-18 21:03:23 -0300

    Change a docstring

--- src/convert.lisp
+++ src/convert.lisp
@@ -10,7 +10,7 @@
 
 (defgeneric lisp-to-database (database value)
   (:documentation "Convert a Lisp value to a value appropriate for the given
-  database, given the SQL type."))
+  database."))
 
 (defgeneric database-to-lisp (database value type)
   (:documentation "Convert a database value to a Lisp value, given the SQL
